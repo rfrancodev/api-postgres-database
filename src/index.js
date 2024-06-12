9956232df5751241110b810aa11582c4b9d1e605
Rafael <francorfs@icloud.com>
2024-06-11 21:28:22 -0300

    Rotas patch e  delete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.post('/session',async (req, res) => {
     }
 })
 
-app.post('/petong/user_id', async(req, res) =>{
+app.post('/petong/:user_id', async(req, res) =>{
     const {description, done} = req.body
     const {user_id} = req.params
     try {
@@ -50,7 +50,7 @@ app.post('/petong/user_id', async(req, res) =>{
     }
 })
 
-app.get('/petong/user_id', async (req, res)=>{
+app.get('/petong/:user_id', async (req, res)=>{
     const {user_id} = req.params
     try {
         const allPetong = await pool.query('SELECT * FROM petong WHERE user_id = ($1)', [user_id])
@@ -60,6 +60,36 @@ app.get('/petong/user_id', async (req, res)=>{
         return res.status(400).send(error)
     }
 })
+
+app.patch('/petong/user_id/:petong_id', async(req,res)=>{
+    const {petong_id, user_id}= re.params
+    const data = req.body
+    
+    try {
+        const belongToUser = await pool.query('SELECT * FROM petong WHERE user_id = ($1) AND petong_id = ($2)', [user_id, petong_id])
+        if (!belongToUser.rows[0]) return res.status(400).send('Operation not allowed')
+
+        const updatePetong = await pool.query('UPDATE petong SET petong_description = ($1), petong_done = ($2), WHERE pentong_id = ($3) RETURNING *', [data.description, data.done, petong_id])
+        return res.status(200).send(updatePetong.rows)
+        
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+})
+
+app.delete('/petong/:user_id/:petong_id', async(req, res)=>{
+    const {user_id, petong_id}= req.params
+    try {
+        const deletarPetong = await pool.query('DELETE FROM petong WHERE petong_id = ($1) RETURNING *', [petong_id])
+        return res.status(200).send({
+            message: 'Usuario deletado com sucesso', 
+            deletarPetong
+        })
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+})
+
 
 app.listen(PORT, () => console.log(
     `Servidor rodando na porta ${PORT}`))
